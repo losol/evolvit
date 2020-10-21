f64641ad2186b4ea1b01c34a9aea40d6c2bc9b67
Ole Kristian Losvik <ole@losol.no>
2020-10-21 19:32:36 +0200

    Use AWS S3 for files

---
+++ config/env/production/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_S3__ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_S3__ACCESS_SECRET"),
+      region: env("AWS_S3__REGION"),
+      params: {
+        Bucket: env("AWS_S3__ACCESS_KEY_ID"),
+      },
+    },
+  },
+});
